6889424a5a3ff5ae6b784a7bdcb3acf2eac2bc4b
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-07 17:28:18 +0530

    Linking Pages with Url

--- helloworld/src/hellowoorld/web/urls.py
+++ helloworld/src/hellowoorld/web/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from web.views import index, about
 
 
+app_name = "web"
+
+
 urlpatterns = [
-    path("",index),
-    path("about/",about)
+    path("",index,name="index"),
+    path("about/",about,name="about")
 ]
