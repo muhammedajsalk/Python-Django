7e7e887176b8559db7619ce2d9c226048e81dd30
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-07 10:31:36 +0530

    Writing view function

--- helloworld/src/hellowoorld/hellowoorld/urls.py
+++ helloworld/src/hellowoorld/hellowoorld/urls.py
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.http.response import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello world")
+from web.views import index
 
 
 urlpatterns = [
--- helloworld/src/hellowoorld/web/views.py
+++ helloworld/src/hellowoorld/web/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
+
+def index(request):
+    return HttpResponse("Hello world")
 # Create your views here.
